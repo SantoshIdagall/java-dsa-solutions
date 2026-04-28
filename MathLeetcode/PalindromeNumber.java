8a2e3dcc0434cdd4af894858757e6c480b5eaec5
Santosh Idagall <santuidagall18@gmail.com>
2026-04-28 22:25:01 +0530

    add PalindromeNumber

---
+++ MathLeetcode/PalindromeNumber.java
@@ -0,0 +1,25 @@
+package MathLeetcode;
+
+public class PalindromeNumber {
+
+    public static boolean ispalindrome(int num){
+        if(num < 0) return false;
+        int orgnum = num;
+
+        int rev= 0;
+        while (num > 0) {
+             int digit= num%10;
+             rev = rev*10 + digit;
+             num/=10;
+             
+        }
+         return orgnum == rev;
+
+    }
+  public static void main(String[] args) {
+    int num =  121;
+     System.out.println(ispalindrome(num));
+  }
+
+
+}
